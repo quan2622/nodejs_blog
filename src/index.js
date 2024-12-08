a39d6c7f0feb1f58230cefd12e54bcfd0d11258c
Quan Nguyen <nhq367821@gmail.com>
2024-12-08 15:52:09 +0700

    remove /public/sss

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const express = require('express');
+const {engine} = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+// Xử lý static file
+app.use(express.static(path.join(__dirname, 'public')));
+
+//template engine
+app.engine('hbs', engine({
+    extname: '.hbs',
+  })
+)
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
