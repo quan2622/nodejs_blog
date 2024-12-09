149c30fdc82144c4d2f02767e01de62ff248638b
Quan Nguyen <nhq367821@gmail.com>
2024-12-09 20:14:01 +0700

    refactor code: create router, app/controller folder

--- src/index.js
+++ src/index.js
@@ -4,24 +4,29 @@ const {engine} = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+
+const route = require('./routes');
 // Xử lý static file
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({
+  extended: true
+}));
+// middleware để xữ lý dữ liệu dùng phương thức post
+app.use(express.json());
+
 
 //template engine
 app.engine('hbs', engine({
     extname: '.hbs',
   })
 )
-app.set('view engine', 'hbs')
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
+// Routes init
+route(app);
 
-app.get('/news', (req, res) => {
-  res.render('news');
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
